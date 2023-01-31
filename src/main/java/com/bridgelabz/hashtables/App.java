6f400616e9e19f3af0638157ba2fb045d5a5d267
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2023-01-30 19:24:12 -0800

    Add:Welcome to the Hash Tables Program

---
+++ src/main/java/com/bridgelabz/hashtables/App.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.hashtables;
+
+public class App 
+{
+    public static void main( String[] args ){
+        
+        System.out.println( "WELCOME TO THE HASH TABLES" );
+    }
+}
